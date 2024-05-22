c6f48d891b795d9da68b567b1d38a8b95e28d565
Chad Hovey <hovey@me.com>
2024-05-22 11:52:47 -0400

    finish command line entry

--- python/command_line/mypackage/command_line.py
+++ python/command_line/mypackage/command_line.py
@@ -1,5 +1,36 @@
 """Illustration of command line entry points."""
 
+from typing import Final
+
+BANNER: Final[
+    str
+] = """----------------------------
+This is the mypackage module
+----------------------------
+"""
+
+
+CLI_DOCS: Final[
+    str
+] = """mypackage
+    (this command)
+
+hello
+    Runs the 'Hello World!' example.
+
+pytest
+    Runs the test suite (non-verbose option).
+
+pytest -v
+    Runs the test suite (verbose option).
+"""
+
+
+def mymodule():
+    """Prints the command line documentation to the command window."""
+    print(BANNER)
+    print(CLI_DOCS)
+
 
 def hello() -> str:
     """Simple example of a function hooked to a command line entry point.
