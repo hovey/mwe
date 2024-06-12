754dd133200796c66c8d0d7c33d7df45e8ab00e3
Chad Hovey <hovey@me.com>
2024-06-12 16:22:29 -0400

    union of test coverage

---
+++ python/coverage_combo/source_1/test_hello.py
@@ -0,0 +1,23 @@
+# to generate the coverage report
+# pytest --cov=.
+# pytest --cov=. --cov-report=html
+
+
+def hello() -> str:
+    """Hello world in English."""
+    return "Hello World!"
+
+
+def hola() -> str:
+    """Hello world in Spanish."""
+    return "Hola Mundo!"
+
+
+# in source_1 only
+def test_hello():
+    assert hello() == "Hello World!"
+
+
+# in source_2 only
+# def test_hola():
+#     assert hola() == "Hola Mundo!"
