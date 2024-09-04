afa04de91765c781b36d98ee41fb188c4f0c2717
Chad Hovey <hovey@me.com>
2024-09-04 17:17:26 -0400

    lattice mesh to element mesh development

--- python/cicd_release/cicd_example/command_line.py
+++ python/cicd_release/cicd_example/command_line.py
@@ -40,3 +40,56 @@ def hello() -> str:
     """
 
     return "Hello world!"
+
+
+def renumber(source: tuple, old: tuple, new: tuple) -> tuple:
+    """Given a source tuple, composed of a list of positive integers,
+    a tuple of `old` numbers that maps into `new` numbers, return the
+    source tuple with the `new` numbers."""
+    result = ()
+    for item in source:
+        idx = old.index(item)
+        new_value = new[idx]
+        result = result + (new_value,)
+
+    return result
+
+
+def mesh_element_connectivity(mesh_lattice_connectivity: tuple):
+    """Given a mesh with lattice connectivity, return a mesh with finite
+    element connectivity.
+    """
+    # create a list of unordered lattice node numbers
+    ln = []
+    for item in mesh_lattice_connectivity:
+        print(f"item is {item}")
+        # The first item is the block number
+        # block = item[0]
+        # The second and onward items are the elements
+        elements = item[1:]
+        for element in elements:
+            ln += list(element)
+
+    ln_set = set(ln)  # sets are not necessarily ordered
+    ln_ordered = tuple(sorted(ln_set))  # now these unique integers are ordered
+
+    # and they will map into the new compressed unique interger list `mapsto`
+    mapsto = tuple(range(1, len(ln_ordered)+1))
+
+    # now build a mesh_with_element_connectivity
+    mesh = ()  # empty tuple
+    # breakpoint()
+    for item in mesh_lattice_connectivity:
+        # The first item is the block number
+        block_number = item[0]
+        block_and_elements = ()  # empty tuple
+        # insert the block number
+        block_and_elements = block_and_elements + (block_number,)
+        for element in item[1:]:
+            new_element = renumber(source=element, old=ln_ordered, new=mapsto)
+            # overwrite
+            block_and_elements = block_and_elements + (new_element,)
+
+        mesh = mesh + (block_and_elements,)  # overwrite
+
+    return mesh
--- python/cicd_release/tests/test_command_line.py
+++ python/cicd_release/tests/test_command_line.py
@@ -7,10 +7,10 @@ Example:
     source .venv/bin/activate.fish
 
     pytest (without code coverage)
-    pytest mypackage/tests/test_command_line.py -v  # -v is for verbose
+    pytest tests/test_command_line.py -v  # -v is for verbose
 
     to run just a single test in this module, for example
-    pytest command_line/tests/test_command_line.py::test_hello_world -v
+    pytest tests/test_command_line.py::test_hello_world -v
 """
 
 from cicd_example import command_line as cl
@@ -24,3 +24,63 @@ def test_hello_world():
     found = cl.hello()
 
     assert found == fiducial
+
+
+def test_renumber():
+    """Tests that the renumber function works as expected."""
+    source = (300, 22, 1)
+    old = (1, 22, 300, 40)
+    new = (42, 2, 9, 1000)
+
+    result = cl.renumber(source=source, old=old, new=new)
+    assert result == (9, 2, 42)
+
+
+def test_mesh_with_element_connectivity():
+    """Test CubeMulti by hand."""
+    gold_mesh_lattice_connectivity = (
+        (
+            2,
+            (2, 3, 6, 5, 11, 12, 15, 14),
+            (4, 5, 8, 7, 13, 14, 17, 16),
+            (5, 6, 9, 8, 14, 15, 18, 17),
+        ),
+        (
+            31,
+            (11, 12, 15, 14, 20, 21, 24, 23),
+        ),
+        (
+            44,
+            (14, 15, 18, 17, 23, 24, 27, 26),
+        ),
+        (
+            82,
+            (1, 2, 5, 4, 10, 11, 14, 13),
+        ),
+    )
+    gold_mesh_element_connectivity = (
+        (
+            2,
+            (2, 3, 6, 5, 11, 12, 15, 14),
+            (4, 5, 8, 7, 13, 14, 17, 16),
+            (5, 6, 9, 8, 14, 15, 18, 17)
+        ),
+        (
+            31,
+            (11, 12, 15, 14, 19, 20, 22, 21)
+        ),
+        (
+            44,
+            (14, 15, 18, 17, 21, 22, 24, 23)
+        ),
+        (
+            82,
+            (1, 2, 5, 4, 10, 11, 14, 13)
+        )
+    )
+
+    result = cl.mesh_element_connectivity(
+        mesh_lattice_connectivity=gold_mesh_lattice_connectivity
+    )
+
+    assert result == gold_mesh_element_connectivity
